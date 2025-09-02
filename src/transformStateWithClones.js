1e144cf741375887fd0cfcc89d2e044ef7b8815d
Aleksandra Chomicz <chomiczaa@gmail.com>
2025-09-02 16:50:09 +0200

    fix

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -29,7 +29,6 @@ function transformStateWithClones(state, actions) {
         break;
 
       default:
-        throw new Error(`Unknown action type: ${action.type}`);
     }
 
     history.push({ ...currentState });
